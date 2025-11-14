42c7d357039c8b4c79c8c0ed360513bc7d105185
r4lax001 <relaxzrl1@gmail.com>
2025-11-15 00:30:39 +0700

    delete

--- src/components/ProtectedRoute.jsx
+++
@@ -1,9 +0,0 @@
-import { Navigate, Outlet } from 'react-router-dom'
-
-function ProtectedRoute() {
-  const isLoggedIn = localStorage.getItem('isLoggedIn')
-  
-  return isLoggedIn === 'true' ? <Outlet /> : <Navigate to='/login' replace />
-}
-
-export default ProtectedRoute
